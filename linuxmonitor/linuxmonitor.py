3fcd92c6d8ca9de499db13e95c2b5ed8a16aca9d
Quentin CG <quentin@comte-gaz.com>
2024-09-14 17:38:35 +0200

    version 1.1.0

--- linuxmonitor/linuxmonitor.py
+++ linuxmonitor/linuxmonitor.py
@@ -28,7 +28,7 @@ __email__ = "quentin@comte-gaz.com"
 __license__ = "MIT License"
 __copyright__ = "Copyright Quentin Comte-Gaz (2024)"
 __python_version__ = "3.+"
-__version__ = "1.0.6 (2024/09/14)"
+__version__ = "1.1.0 (2024/09/14)"
 __status__ = "Usable for any Linux project"
 
 import json
